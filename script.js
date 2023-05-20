39798cc7a17d52edaee8986ea64c4f4499e83cc2
zeeshan <zeeshanshafiq84>
2023-05-20 09:46:33 -0700

    =sixth commit

--- script.js
+++ script.js
@@ -1,25 +1,131 @@
 const canvas = document.querySelector("canvas"),
+toolBtns = document.querySelectorAll(".tool"),
+fillColor = document.querySelector("#fill-color"),
+sizeSlider = document.querySelector("#size-slider"),
+colorBtns = document.querySelectorAll(".colors .option"),
+colorPicker = document.querySelector("#color-picker"),
+clearCanvas = document.querySelector(".clear-canvas"),
+saveImg = document.querySelector(".save-img"),
 ctx = canvas.getContext("2d");
 
-let isDrawing = false;
+// Global variables with default value
+let prevMouseX , prevMouseY, snapshot,
+isDrawing = false,
+selectedTool = "brush",
+brushWidth = 5,
+selectedColor ="#000";
 
-window.addEventListener("load",() => {
+const setCanvasBackground = () => {
+
+    ctx.fillStyle = "#fff";
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    ctx.fillStyle = selectedColor; // setting fillstyle back to the selectedColor, it'll be the brush color
+}
+
+window.addEventListener("load", () => {
     // setting canvas width /height... offset width/height returns viewable width/height of an element
     canvas.width = canvas.offsetWidth;
     canvas.height = canvas.offsetHeight;
-
+    setCanvasBackground();
 
 });
 
-const startDraw = () => {
+const drawRect = (e) => {
+    // if fill color isn't checked draw a rect with border else draw rect with background 
+    if(!fillColor.checked) {
+        // creating cricle according to the mouse pointer
+        return ctx.strokeRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
+    }
+    ctx.fillRect(e.offsetX, e.offsetY, prevMouseX - e.offsetX, prevMouseY - e.offsetY);
+}
+
+const drawCircle = (e) => {
+    ctx.beginPath();  // Creating new path to draw circle
+    // Getting radius for circle according to the mouse pointer
+    let radius = Math.sqrt(Math.pow((prevMouseX - e.offsetX), 2) + Math.pow((prevMouseY - e.offsetY), 2));
+    ctx.arc(prevMouseX, prevMouseY, radius, 0, 2 * Math.PI);
+    fillColor.checked ? ctx.fill() : ctx.stroke();
+}
+
+const drawTriangle = (e) => {
+    ctx.beginPath();  // Creating new path to draw circle
+    ctx.moveTo(prevMouseX, prevMouseY); 
+    ctx.lineTo(e.offsetX, e.offsetY);
+    ctx.lineTo(prevMouseX * 2 - e.offsetX, e.offsetY);
+    ctx.closePath();
+    fillColor.checked ? ctx.fill() : ctx.stroke();
+}
+
+const startDraw = (e) => {
     isDrawing = true;
+    prevMouseX = e.offsetX; // passing mouse current position as prevMouseX value
+    prevMouseY = e.offsetY; // passing mouse current position as prevMouseY value
+    ctx.beginPath();  // Creating new path to draw
+    ctx.lineWidth = brushWidth; // Passing brush size as line width
+    ctx.strokeStyle = selectedColor;
+    ctx.fillStyle = selectedColor;
+    // copying canvas data & passing as snapshot value .. this avoids dragging the image   
+    snapshot = ctx.getImageData(0, 0, canvas.width, canvas.height);
 }
 
-const  drawing = (e) => {
+const drawing = (e) => {
     if(!isDrawing) return; // if isDrawing is false return from here 
-    ctx.lineTo(e.offsetX, e.offsetY); // Creating line according to the mouse pointer 
-    ctx.stroke(); // drawing / filling line with color
+    ctx.putImageData(snapshot, 0, 0); // adding copied canvas data on this canvas
+    
+    if(selectedTool === "brush" || selectedTool === "eraser") {
+
+        ctx.strokeStyle = selectedTool === "eraser" ? "#fff" : selectedColor;
+        ctx.lineTo(e.offsetX, e.offsetY); // Creating line according to the mouse pointer 
+        ctx.stroke(); // drawing / filling line with color
+    } else if(selectedTool === "rectangle"){
+        drawRect(e);
+    } else if(selectedTool === "circle"){
+        drawCircle(e);
+    } else {
+     drawTriangle(e);
+    
 }
 
-canvas.addEventListener("mousedown" ,startDraw);
-canvas.addEventListener("mousemove" ,drawing);
+}
+
+toolBtns.forEach(btn => {
+    btn.addEventListener("click", () => {  // Adding click event to all tool option
+        // removing active class from the previous option and adding on current clicked option
+        document.querySelector(".options .active").classList.remove("active");
+        btn.classList.add("active");
+        selectedTool = btn.id;
+        
+
+    });
+});
+
+sizeSlider.addEventListener("change", () => brushWidth = sizeSlider.value);
+
+colorBtns.forEach(btn =>{
+    btn.addEventListener("click", () =>{
+        document.querySelector(".options ,selected").classList.remove("selected");
+        btn.classList.add("selected");
+        selectedColor = window.getComputedStyle(btn).getPropertyValue("background-color");
+    });
+});
+
+colorPicker.addEventListener("change", () => {
+    colorPicker.parentElement.style.background = colorPicker.value;
+    colorPicker.parentElement.click();
+});
+
+clearCanvas.addEventListener("click", () => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    setCanvasBackground();
+});
+
+saveImg.addEventListener("click", () => {
+    const link = document.createElement("a");
+    link.download = `${Date.now()}.jpg`;
+    link.href = canvas.toDataURL();
+    link.click();
+})
+
+canvas.addEventListener("mousedown" , startDraw);
+canvas.addEventListener("mousemove" , drawing);
+canvas.addEventListener("mouseup" , () => isDrawing = false);
